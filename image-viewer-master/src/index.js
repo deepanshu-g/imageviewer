9655a23f368f0a8f32fda43c5e6ca9a4cbc81581
Aravind KR <kraravind1993@gmail.com>
2019-05-31 20:20:02 +0530

    Updated index.js

--- image-viewer-master/src/index.js
+++ image-viewer-master/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './screens/login/Login';
+import Controller from './Controller';
+
 
 ReactDOM.render(
-    <Login />, 
+    <Controller />,
     document.getElementById('root')
 );

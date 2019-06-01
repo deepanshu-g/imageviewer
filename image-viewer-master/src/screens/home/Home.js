90af0395f1f300c0aac658edca00669ab9cbcd17
pillella <44552477+pillella@users.noreply.github.com>
2019-06-01 14:54:16 +0530

    Added script for Home

--- image-viewer-master/src/screens/home/Home.js
+++ image-viewer-master/src/screens/home/Home.js
@@ -57,3 +57,160 @@ const styles =  theme => ({
     marginTop:90
   }
 });
+
+class Home extends Component{
+
+  constructor(props) {
+    super(props);
+    if (sessionStorage.getItem('access-token') == null) {
+      props.history.replace('/');
+    }
+    this.state = {
+      data: [],
+      filteredData:[],
+      userData:{},
+      likeSet:new Set(),
+      comments:{},
+      currrentComment:""
+    }
+  }
+
+  componentDidMount(){
+    this.getUserInfo();
+    this.getMediaData();
+  }
+
+  render(){
+    const{classes} = this.props;
+    return(
+      <div>
+        <Header
+          userProfileUrl={this.state.userData.profile_picture}
+          screen={"Home"}
+          searchHandler={this.onSearchEntered}
+          handleLogout={this.logout}
+          handleAccount={this.navigateToAccount}/>
+        <div className={classes.grid}>
+          <GridList className={classes.gridList} cellHeight={'auto'}>
+            {this.state.filteredData.map(item => (
+              <GridListTile key={item.id}>
+                <HomeItem
+                  classes={classes}
+                  item={item}
+                  onLikedClicked={this.likeClickHandler}
+                  onAddCommentClicked={this.addCommentClickHandler}
+                  commentChangeHandler={this.commentChangeHandler}
+                  comments={this.state.comments}/>
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </div>
+    );
+  }
+
+  onSearchEntered = (value) =>{
+    console.log('search value', value);
+    let filteredData = this.state.data;
+    filteredData = filteredData.filter((data) =>{
+      let string = data.caption.text.toLowerCase();
+      let subString = value.toLowerCase();
+      return string.includes(subString);
+    })
+    this.setState({
+      filteredData
+    })
+  }
+
+  likeClickHandler = (id) =>{
+    console.log('like id',id);
+    var foundItem = this.state.data.find((item) => {
+      return item.id === id;
+    })
+
+    if (typeof foundItem !== undefined) {
+      if (!this.state.likeSet.has(id)) {
+        foundItem.likes.count++;
+        this.setState(({likeSet}) => ({
+          likeSet:new Set(likeSet.add(id))
+        }))
+      }else {
+        foundItem.likes.count--;
+        this.setState(({likeSet}) =>{
+          const newLike = new Set(likeSet);
+          newLike.delete(id);
+
+          return {
+            likeSet:newLike
+          };
+        });
+      }
+    }
+  }
+
+  addCommentClickHandler = (id)=>{
+    if (this.state.currentComment === "" || typeof this.state.currentComment === undefined) {
+      return;
+    }
+
+    let commentList = this.state.comments.hasOwnProperty(id)?
+      this.state.comments[id].concat(this.state.currentComment): [].concat(this.state.currentComment);
+
+    this.setState({
+      comments:{
+        ...this.state.comments,
+        [id]:commentList
+      },
+      currentComment:''
+    })
+  }
+
+
+  commentChangeHandler = (e) => {
+    this.setState({
+      currentComment:e.target.value
+    });
+  }
+
+  getUserInfo = () => {
+    let that = this;
+    let url = `${constants.userInfoUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        userData:jsonResponse.data
+      });
+    }).catch((error) => {
+      console.log('error user data',error);
+    });
+  }
+
+  getMediaData = () => {
+    let that = this;
+    let url = `${constants.userMediaUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        data:jsonResponse.data,
+        filteredData:jsonResponse.data
+      });
+    }).catch((error) => {
+      console.log('error user data',error);
+    });
+  }
+
+  logout = () => {
+    sessionStorage.clear();
+    this.props.history.replace('/');
+  }
+
+  navigateToAccount = () =>{
+    this.props.history.push('/profile');
+  }
+}

a6284b53a3e3ba2c46e844fb12e249cd44ebce80
pillella <44552477+pillella@users.noreply.github.com>
2019-06-01 14:55:10 +0530

    Added Script for HomeItem

--- image-viewer-master/src/screens/home/Home.js
+++ image-viewer-master/src/screens/home/Home.js
@@ -214,3 +214,130 @@ class Home extends Component{
     this.props.history.push('/profile');
   }
 }
+
+
+class HomeItem extends Component{
+  constructor(){
+    super();
+    this.state = {
+      isLiked : false,
+      comment:'',
+    }
+  }
+
+  render(){
+    const {classes, item, comments} = this.props;
+
+    let createdTime = new Date(0);
+    createdTime.setUTCSeconds(item.created_time);
+    let yyyy = createdTime.getFullYear();
+    let mm = createdTime.getMonth() + 1;
+    let dd = createdTime.getDate();
+
+    let HH = createdTime.getHours();
+    let MM = createdTime.getMinutes();
+    let ss = createdTime.getSeconds();
+
+    let time = dd+"/"+mm+"/"+yyyy+" "+HH+":"+MM+":"+ss;
+    let hashTags = item.tags.map(hash =>{
+      return "#"+hash;
+    });
+    return(
+      <div className="home-item-main-container">
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar alt="User Profile Pic" src={item.user.profile_picture} className={classes.avatar}/>
+            }
+            title={item.user.username}
+            subheader={time}
+          />
+          <CardContent>
+            <CardMedia
+              className={classes.media}
+              image={item.images.standard_resolution.url}
+              title={item.caption.text}
+            />
+            <div  className={classes.hr}>
+              <Typography component="p">
+                {item.caption.text}
+              </Typography>
+              <Typography style={{color:'#4dabf5'}} component="p" >
+                {hashTags.join(' ')}
+              </Typography>
+            </div>
+          </CardContent>
+
+            <CardActions>
+              <IconButton aria-label="Add to favorites" onClick={this.onLikeClicked.bind(this,item.id)}>
+                {this.state.isLiked && <FavoriteIconFill style={{color:'#F44336'}}/>}
+                {!this.state.isLiked && <FavoriteIconBorder/>}
+              </IconButton>
+              <Typography component="p">
+                {item.likes.count} Likes
+              </Typography>
+            </CardActions>
+
+            <CardContent>
+            {comments.hasOwnProperty(item.id) && comments[item.id].map((comment, index)=>{
+              return(
+                <div key={index} className="row">
+                  <Typography component="p" style={{fontWeight:'bold'}}>
+                    {sessionStorage.getItem('username')}:
+                  </Typography>
+                  <Typography component="p" >
+                    {comment}
+                  </Typography>
+                </div>
+              )
+            })}
+            <div className={classes.formControl}>
+              <FormControl style={{flexGrow:1}}>
+                <InputLabel htmlFor="comment">Add Comment</InputLabel>
+                <Input id="comment" value={this.state.comment} onChange={this.commentChangeHandler}/>
+              </FormControl>
+              <FormControl>
+                <Button onClick={this.onAddCommentClicked.bind(this,item.id)}
+                   variant="contained" color="primary">
+                  ADD
+                </Button>
+              </FormControl>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
+  onLikeClicked = (id) => {
+    if (this.state.isLiked) {
+      this.setState({
+        isLiked:false
+      });
+    }else {
+      this.setState({
+        isLiked:true
+      });
+    }
+    this.props.onLikedClicked(id)
+  }
+
+  commentChangeHandler = (e) => {
+    this.setState({
+      comment:e.target.value,
+    });
+    this.props.commentChangeHandler(e);
+  }
+
+  onAddCommentClicked = (id) => {
+    if (this.state.comment === "" || typeof this.state.comment === undefined) {
+      return;
+    }
+    this.setState({
+      comment:""
+    });
+    this.props.onAddCommentClicked(id);
+  }
+}
+
+export default withStyles(styles)(Home);
